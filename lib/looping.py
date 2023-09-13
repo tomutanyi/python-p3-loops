4f3bd0c5d14d469beced01b91a6379239c0d1dd7
tomutanyi <tomutanyi@gmail.com>
2023-09-13 20:05:25 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,33 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    count = 10
+    while count >= 1:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+# code goes here!
+pass
 
 def square_integers(int_list):
+    new_list = []
+    for i in int_list:
+        new_list.append(i ** 2)
+    return new_list
     # code goes here!
     pass
 
 def fizzbuzz():
+    for i in range (1,101):
+        if i % 15 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
+fizzbuzz()
+
     # code goes here!
-    pass
